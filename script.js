67eee4d126b331cad2f6c4d29e766bc49e837c64
Isak Åkerblad <isak.akerblad@elev.ga.ntig.se>
2026-05-08 21:33:25 +0200

    sparar

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function popup() {
-    document.querySelector(".undertext").style.display="block"
+// bild1
+function popup1() {
+    document.querySelector(".undertext1").style.display="block"
 }
-function popout() {
-    document.querySelector(".undertext").style.display="none"
+function popout1() {
+    document.querySelector(".undertext1").style.display="none"
+}
+// bild3
+function popup3() {
+    document.querySelector(".undertext3").style.display="block"
+}
+function popout3() {
+    document.querySelector(".undertext3").style.display="none"
+}
+// bild5
+function popup5() {
+    document.querySelector(".undertext5").style.display="block"
+}
+function popout5() {
+    document.querySelector(".undertext5").style.display="none"
+}
+// bild2
+function popup2() {
+    document.querySelector(".undertext2").style.display="block"
+}
+function popout2() {
+    document.querySelector(".undertext2").style.display="none"
+}
+// bild4
+function popup4() {
+    document.querySelector(".undertext4").style.display="block"
+}
+function popout4() {
+    document.querySelector(".undertext4").style.display="none"
+}
+// bild2
+function popup6() {
+    document.querySelector(".undertext6").style.display="block"
+}
+function popout6() {
+    document.querySelector(".undertext6").style.display="none"
 }
