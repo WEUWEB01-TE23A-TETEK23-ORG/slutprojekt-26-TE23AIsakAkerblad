a928fe61cfd52f4fdd5bbc11c9015bc7bbdd4404
Isak Åkerblad <isak.akerblad@elev.ga.ntig.se>
2026-05-10 14:20:06 +0200

    klar nu, fixade alla småsaker och koppierade min infosida med ny information för få mina resterande sidor

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 // bild1
 function popup1() {
-    document.querySelector(".undertext1").style.display="block"
+    document.querySelector(".undertext1").style.display = "block"
 }
 function popout1() {
-    document.querySelector(".undertext1").style.display="none"
+    document.querySelector(".undertext1").style.display = "none"
 }
 // bild3
 function popup3() {
-    document.querySelector(".undertext3").style.display="block"
+    document.querySelector(".undertext3").style.display = "block"
 }
 function popout3() {
-    document.querySelector(".undertext3").style.display="none"
+    document.querySelector(".undertext3").style.display = "none"
 }
 // bild5
 function popup5() {
-    document.querySelector(".undertext5").style.display="block"
+    document.querySelector(".undertext5").style.display = "block"
 }
 function popout5() {
-    document.querySelector(".undertext5").style.display="none"
+    document.querySelector(".undertext5").style.display = "none"
 }
 // bild2
 function popup2() {
-    document.querySelector(".undertext2").style.display="block"
+    document.querySelector(".undertext2").style.display = "block"
 }
 function popout2() {
-    document.querySelector(".undertext2").style.display="none"
+    document.querySelector(".undertext2").style.display = "none"
 }
 // bild4
 function popup4() {
-    document.querySelector(".undertext4").style.display="block"
+    document.querySelector(".undertext4").style.display = "block"
 }
 function popout4() {
-    document.querySelector(".undertext4").style.display="none"
+    document.querySelector(".undertext4").style.display = "none"
 }
 // bild2
 function popup6() {
-    document.querySelector(".undertext6").style.display="block"
+    document.querySelector(".undertext6").style.display = "block"
 }
 function popout6() {
-    document.querySelector(".undertext6").style.display="none"
+    document.querySelector(".undertext6").style.display = "none"
+}
+// start1
+function start1() {
+    document.querySelector(".japan").style.display = "block"
+}
+function slut1() {
+    document.querySelector(".japan").style.display = "none"
+}
+// start2
+function start2() {
+    document.querySelector(".sverige").style.display = "block"
+}
+function slut2() {
+    document.querySelector(".sverige").style.display = "none"
+}
+// start3
+function start3() {
+    document.querySelector(".sverige").style.display = "block"
+}
+function slut3() {
+    document.querySelector(".sverige").style.display = "none"
 }
